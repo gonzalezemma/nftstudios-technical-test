dc050a95bebaea319a5257358db24cd26fe89430
emgonzalez <emgonzalez@indec.gob.ar>
2021-08-30 23:33:03 -0300

    develop(app): add useEffect to get nfts array at first render and add style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 
-import './App.css';
+import { useEffect, useState } from 'react';
+import { map } from 'lodash';
 import getNtfs from './service/nfts';
+import './App.css';
 
-getNtfs().then(res => console.log(res.assets));
 
 function App() {
+  const [ntfs, setNtfs] = useState([])
+
+  useEffect(() => {
+    getNtfs().then(res => setNtfs(res.assets));
+  }, [])
+  
   return (
     <div className="App">
-        
+      <div className="NtfsList">
+        {map(ntfs, ntf => (
+          <div key={ntf.id} className="NtfsList-item"> 
+              <img src={ntf.image_preview_url} alt={ntf.name}/>
+              <h6>{ntf.name}</h6>
+          </div>
+        ))}
+        </div>
     </div>
   );
 }
